7652159301a68233958703807c66560921cd9faf
marica <2frontend.dev@gmail.com>
2024-02-27 20:23:24 +0300

    fix: fix favorites page

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -67,7 +67,7 @@ export const FavoritesPage = () => (
                     </div>
                     <div className="place-card__rating rating">
                       <div className="place-card__stars rating__stars">
-                        <span style="width: 100%"></span>
+                        <span style={{'width': '100%'}}></span>
                         <span className="visually-hidden">Rating</span>
                       </div>
                     </div>
